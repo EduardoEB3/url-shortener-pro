26b5c414a1556337f83ace63b2bcec8fb8def08c
Eduardo Expósito Barrera <eduardoexba3@gmail.com>
2026-05-08 20:12:45 +0100

    feat: :sparkles: Adding endpoint redirect

--- src/controllers/url.controller.ts
+++ src/controllers/url.controller.ts
@@ -16,4 +16,24 @@ export class UrlController {
       return res.status(500).json({ error: "Internal Server Error" });
     }
   }
+
+  static async redirect(req: Request, res: Response): Promise<void | Response> {
+    try {
+      const { shortCode } = req.params;
+
+      if (!shortCode) {
+        return res.status(400).json({ error: "Short code is required" });
+      }
+
+      const originalUrl = await UrlService.getOriginalUrl(shortCode as string);
+
+      if (originalUrl) {
+        return res.redirect(originalUrl);
+      }
+
+      return res.status(404).json({ error: "URL not found" });
+    } catch (error) {
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
 }
--- src/routes/url.routes.ts
+++ src/routes/url.routes.ts
@@ -5,5 +5,6 @@ import { validateUrl } from "../middlewares/validateUrl";
 const urlRouter = Router();
 
 urlRouter.post("/create", validateUrl, UrlController.create);
+urlRouter.get("/:shortCode", UrlController.redirect);
 
 export default urlRouter;
